63d0f1c553cb4f49724144903221250da10da3a6
peter <peter>
2023-01-08 14:55:05 -0500

    Initial commit

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1673207617,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/cart-sheet0.png",
+		"images/cart-sheet1.png",
+		"images/tree-sheet0.png",
+		"images/apple-sheet0.png",
+		"images/apple-sheet1.png",
+		"images/sprite4-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/anvil-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"images/tiledbackground4.png",
+		"images/spritefont.png",
+		"media/cart song.ogg",
+		"media/cart lose.ogg",
+		"media/main2.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
